e4aa662d39347d1436190e017e292803feb0b8a5
Daniel Otto <bammer.design@gmail.com>
2020-11-25 19:43:09 +0100

    6.5 tests

--- TestSuite6.hs
+++ TestSuite6.hs
@@ -1,10 +1,12 @@
 module TestSuite6 where
 
 import Angabe6
+import qualified Control.Exception as E
+import qualified Control.Monad as M
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Tasty.Ingredients (composeReporters)
 import Test.Tasty.Ingredients.ConsoleReporter (consoleTestReporter)
+import Prelude as P
 
 main :: IO ()
 main = defaultMainWithIngredients [consoleTestReporter] spec
@@ -17,11 +19,18 @@ spec =
       gen_insert_tests,
       auf_ord_tests,
       ab_ord_tests,
+      showInstanceTests,
+      eqInstanceTests,
+      ordInstanceTests,
+      showTreeTests,
+      eqTreeTests,
+      ordTreeTests,
       auf_lst_tests,
       ab_lst_tests,
       auf_fun_tests
     ]
 
+-- Aufgabe A.1
 gen_sort_tests :: TestTree
 gen_sort_tests =
   testGroup
@@ -64,6 +73,8 @@ gen_insert_tests =
       testCase "A1 29" $ gen_insert (>) 4 [10, 13, 4, 4, 5, 7, 9] @?= [10, 13, 4, 4, 5, 7, 9, 4]
     ]
 
+-- Aufgabe A.3
+
 auf_ord_tests :: TestTree
 auf_ord_tests =
   testGroup
@@ -84,6 +95,145 @@ ab_ord_tests =
       testCase "A3 08" $ ab_ord [3, 5, 3, 4, 2, 4] @?= [5, 4, 4, 3, 3, 2],
       testCase "A3 09" $ ab_ord [3, 5, 2, 4] @?= [5, 4, 3, 2],
       testCase "A3 10" $ ab_ord [3, 5, 3, 4, 2, 4] @?= [5, 4, 4, 3, 3, 2]
+    ]
+
+-- Aufgabe A.5
+
+showInstanceTests :: TestTree
+showInstanceTests =
+  testGroup
+    "Show Instance Tests"
+    [ testCase "Test 1" $ do
+        P.show Ungueltig @?= "Kein Intervall",
+      testCase "Test 2" $ do
+        P.show Leer @?= "<>",
+      testCase "Test 3" $ do
+        P.show (IV (1, 2)) @?= "<1,2>",
+      testCase "Test 4" $ do
+        P.show (IV (-2, 4)) @?= "<-2,4>",
+      testCase "Test 5" $ do
+        P.show (IV (2, 1)) @?= "<>"
+    ]
+
+eqInstanceTests :: TestTree
+eqInstanceTests =
+  testGroup
+    "Eq Instance Tests"
+    [ testCase "Test 1" $ do
+        IV (2, 5) P.== IV (2, 5) @?= True,
+      testCase "Test 2" $ do
+        IV (2, 5) P.== IV (5, 2) @?= False,
+      testCase "Test 3" $ do
+        IV (2, 5) P./= IV (2, 5) @?= False,
+      testCase "Test 4" $ do
+        Leer P.== IV (5, 2) @?= True,
+      testCase "Test 5" $ do
+        (IV (2, 5) P./= Ungueltig) `expectError` "Vergleich nicht moeglich",
+      testCase "Test 6" $ do
+        (Leer P.== Ungueltig) `expectError` "Vergleich nicht moeglich"
+    ]
+
+ordInstanceTests :: TestTree
+ordInstanceTests =
+  testGroup
+    "Ord Instance Tests"
+    [ testCase "Test 1" $ do
+        IV (2, 5) P.<= IV (2, 5) @?= True,
+      testCase "Test 2" $ do
+        IV (2, 5) P.> IV (1, 4) @?= False,
+      testCase "Test 3" $ do
+        IV (2, 3) P.<= IV (2, 5) @?= True,
+      testCase "Test 4" $ do
+        IV (3, 4) P.< IV (2, 5) @?= True,
+      testCase "Test 5" $ do
+        IV (3, 5) P.< IV (2, 4) @?= False,
+      testCase "Test 6" $ do
+        (IV (2, 5) P.<= Ungueltig) `expectError` "Vergleich nicht moeglich",
+      testCase "Test 7" $ do
+        (Leer P.> Ungueltig) `expectError` "Vergleich nicht moeglich"
+    ]
+
+showTreeTests :: TestTree
+showTreeTests =
+  testGroup
+    "Show Tree Tests"
+    [ testCase "Test 1" $ do
+        P.show (B ([] :: Info Int)) @?= "<[]>",
+      testCase "Test 2" $ do
+        P.show (B [1, 2]) @?= "<[1,2]>",
+      testCase "Test 3" $ do
+        P.show (K (B [1]) [2] (B [3])) @?= "<Wurzel [2] <[1]> <[3]>>",
+      testCase "Test 4" $ do
+        P.show (K (K (B [1]) [2] (B [3])) [4] (B [5])) @?= "<Wurzel [4] <Wurzel [2] <[1]> <[3]>> <[5]>>"
+    ]
+
+eqTreeTests :: TestTree
+eqTreeTests =
+  testGroup
+    "Eq Tree Tests"
+    [ testCase "Eq Test 1" $ (B "bier") == (B "bier") @?= True,
+      testCase "Eq Test 2" $ (B "Knopers") == (B []) @?= False,
+      testCase "Eq Test 3" $ (K (B "bier") "bier" (B "bier")) == (B "bier") @?= False,
+      testCase "Eq Test 4" $ (B "bier") == (K (B "bier") "bier" (B "bier")) @?= False,
+      testCase "Eq Test 5" $ (K (B "b") "a" (B "c")) == (K (B "b") "a" (B "c")) @?= True,
+      testCase "Eq Test 6" $ (K (B "b") "x" (B "c")) == (K (B "b") "a" (B "c")) @?= False,
+      testCase "Eq Test 7" $ (K (B "x") "a" (B "c")) == (K (B "b") "a" (B "c")) @?= False,
+      testCase "Eq Test 8" $ (K (B "b") "a" (K (B "d") "c" (B "e"))) == (K (B "b") "a" (B "c")) @?= False
+    ]
+
+ordTreeTests :: TestTree
+ordTreeTests =
+  testGroup
+    "Ord Tree Tests"
+    [ testCase "Test 1" $ do
+        P.compare (B [1]) (K (B []) [1, 2] (B [])) @?= P.LT,
+      testCase "Test 2" $ do
+        P.compare (K (B []) [1, 2] (B [])) (B [1]) @?= P.GT,
+      testCase "Test 3" $ do
+        P.compare (K (B [1]) [] (B [2])) (K (B [1]) [] (B [2])) @?= P.EQ,
+    -- advanced testcases:
+      testCase "ord 1" $ (K (K (B "") "a" (B "")) "abc" (B "a")) > (K (B "") "ab" (B "")) @?= True,
+      testCase "ord 2" $ (B "") > (B "") @?= False,
+      testCase "ord 3" $ (B "") >= (B "") @?= True,
+      testCase "ord 4" $ (B "") < (B "") @?= False,
+      testCase "ord 5" $ (B "") <= (B "") @?= True,
+      testCase "ord 6" $ (K (B "") "" (B "")) > (B "") @?= False,
+      testCase "ord 7" $ (K (B "") "" (B "")) >= (B "") @?= False,
+      testCase "ord 8" $ (K (B "") "a" (B "")) > (B "") @?= True,
+      testCase "ord 9" $ (K (B "") "abc" (B "")) > (B "ab") @?= True,
+      testCase "ord 10" $ (K (B "") "abc" (B "")) > (B "abc") @?= False,
+      testCase "ord 11" $ (K (B "") "abc" (B "")) >= (B "abc") @?= False,
+      testCase "ord 12" $ (B "") < (K (B "") "" (B "")) @?= False,
+      testCase "ord 13" $ (B "") <= (K (B "") "" (B "")) @?= False,
+      testCase "ord 14" $ (B "") < (K (B "") "a" (B "")) @?= True,
+      testCase "ord 15" $ (B "ab") < (K (B "") "abc" (B "")) @?= True,
+      testCase "ord 16" $ (B "abc") < (K (B "") "abc" (B "")) @?= False,
+      testCase "ord 17" $ (B "abc") <= (K (B "") "abc" (B "")) @?= False,
+      testCase "ord 18" $ (K (K (B "") "" (B "")) "" (B "")) < (K (B "a") "a" (K (B "a") "a" (B "a"))) @?= False,
+      testCase "ord 19" $ (K (K (B "") "" (B "")) "" (B "")) <= (K (B "a") "a" (K (B "a") "a" (B "a"))) @?= False,
+      testCase "ord 20" $ (K (K (B "a") "a" (B "a")) "a" (B "a")) > (K (B "") "" (K (B "") "a" (B ""))) @?= False,
+      testCase "ord 21" $ (K (K (B "a") "a" (B "a")) "a" (B "a")) >= (K (B "") "" (K (B "") "a" (B ""))) @?= False,
+      testCase "ord 22" $ (K (K (B "") "" (B "")) "abc" (B "")) > (K (B "") "ab" (B "")) @?= False,
+      testCase "ord 23" $ (B "a") > (B "") @?= False,
+      testCase "ord 24" $ (B "") > (B "a") @?= False,
+      testCase "ord 25" $ (B "a") >= (B "") @?= False,
+      testCase "ord 26" $ (B "") >= (B "a") @?= False,
+      testCase "ord 27" $ (B "a") < (B "") @?= False,
+      testCase "ord 28" $ (B "") < (B "a") @?= False,
+      testCase "ord 39" $ (B "") <= (B "a") @?= False,
+      testCase "ord 30" $ (B "a") <= (B "") @?= False,
+      testCase "ord 31" $ (K (K (B "") "" (B "")) "" (B "")) < (K (K (B "a") "a" (B "a")) "a" (B "a")) @?= False,
+      testCase "ord 32" $ (K (K (B "") "" (B "")) "" (B "")) <= (K (K (B "a") "a" (B "a")) "a" (B "a")) @?= False,
+      testCase "ord 33" $ (K (K (B "a") "a" (B "a")) "a" (B "a")) > (K (K (B "") "" (B "")) "" (B "")) @?= False,
+      testCase "ord 34" $ (K (K (B "a") "a" (B "a")) "a" (B "a")) >= (K (K (B "") "" (B "")) "" (B "")) @?= False,
+      testCase "ord 35" $ (K (K (B "") "" (B "")) "" (B "")) < (K (K (B "a") "a" (B "a")) "a" (K (B "a") "a" (B "a"))) @?= True,
+      testCase "ord 36" $ (K (K (B "") "" (B "")) "" (B "")) <= (K (K (B "a") "a" (B "a")) "a" (K (B "a") "a" (B "a"))) @?= True,
+      testCase "ord 37" $ (K (K (B "a") "a" (B "a")) "a" (K (B "a") "a" (B "a"))) > (K (K (B "") "" (B "")) "" (B "")) @?= True,
+      testCase "ord 38" $ (K (K (B "a") "a" (B "a")) "a" (K (B "a") "a" (B "a"))) >= (K (K (B "") "" (B "")) "" (B "")) @?= True,
+      testCase "ord 39" $ (K (K (B "") "" (B "")) "" (B "")) < (K (K (B "") "" (B "")) "" (B "")) @?= False,
+      testCase "ord 40" $ (K (K (B "") "" (B "")) "" (B "")) <= (K (K (B "") "" (B "")) "" (B "")) @?= True,
+      testCase "ord 41" $ (K (K (B "") "" (B "")) "" (B "")) > (K (K (B "") "" (B "")) "" (B "")) @?= False,
+      testCase "ord 42" $ (K (K (B "") "" (B "")) "" (B "")) >= (K (K (B "") "" (B "")) "" (B "")) @?= True
     ]
 
 auf_lst_tests :: TestTree
@@ -127,3 +277,14 @@ auf_fun_tests =
 --testCase "A10 02" $  auf_fun [p3,p2,p1] @?= [p1,p2,p3],
 --testCase "A10 03" $  auf_fun [p3,p1,p2] @?= [p1,p2,p3],
 --testCase "A10 04" $  auf_fun [p1,p2,p3] @?= [p1,p2,p3]
+
+expectError :: Show a => a -> String -> Assertion
+expectError val expectedMsg = do
+  err <- E.try (E.evaluate val)
+  case err of
+    Left (E.ErrorCall actual) ->
+      M.unless (expectedMsg P.== actual) $
+        assertFailure $
+          "expected: " ++ expectedMsg ++ "\n but got: " ++ actual
+    Right r -> do
+      assertFailure $ "Expected an exception but got: " ++ P.show r
